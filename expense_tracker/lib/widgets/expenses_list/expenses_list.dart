0548d89dbef3cc8d1eaa52eb116daeec5990e947
Lucas Silva <lucascdsilva.bj@gmail.com>
2024-05-13 21:59:08 -0300

    Criando os componentes da lista e dos itens da lista
    Criação da função de formatação e definição do icons por categoria.

---
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,26 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+import '../../models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList(
+    this.expenses, {
+    super.key,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    // O ListView() serve para gerar a lista na tela com o scroll de forma default,
+    // só que nesse caso todos os elementos da lista são gerados no momento de inicialização,
+    // indepente dos itens serem visiveis ou não.
+    // Já no caso do ListView.builder ele gera a mesma lista com o scroll, sendo que
+    // os itens da lista são gerados sobre demanda, ou seja, caso estejam pertos de ser
+    // visualizados, eles vão sendo instanciados.
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
