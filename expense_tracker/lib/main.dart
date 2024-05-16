bd827da9b096b659eca2b5ae2ebd4af799863988
Lucas Silva <lucascdsilva.bj@gmail.com>
2024-05-15 22:43:15 -0300

    Criação do campo data e internacionalização para BR
    - Criação do campo data usando modal;
    - Configuração para poder fazer internacionalização dos widgets;

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(
     const MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('pt', 'BR')],
       home: Expenses(),
     ),
   );
