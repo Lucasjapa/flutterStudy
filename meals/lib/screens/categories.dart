45569832aa7cf61795ad09d3d75887162439225a
Lucas Silva <lucascdsilva.bj@gmail.com>
2024-05-28 22:09:25 -0300

    Add função para navegar entre as categorias de comida.

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          'Algum titulo',
+          [],
+        ),
+      ),
+    ); // Navigator.push(context, route);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +32,10 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: [
-          for (final category in availableCategories) CategoryGridItem(category)
+          for (final category in availableCategories)
+            CategoryGridItem(category, () {
+              _selectCategory(context);
+            })
         ],
       ),
     );
---
+++ meals/lib/screens/meals.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen(this.title, this.meals, {super.key});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Nenhuma receita adicionada.',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Selecione uma categoria diferente.',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
+  }
+}
